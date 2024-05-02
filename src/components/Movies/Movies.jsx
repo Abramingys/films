f983404574c27cc4b9d2e0ea60311ed7cd6d3f5a
Vladimir Abramovich <abramingys@gmail.com>
2024-05-02 12:05:18 +0300

    installed react-router-dom

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -4,34 +4,33 @@ import MoviesCard from '../MoviesCard/MoviesCard';
 export default function Movies() {
   const [films, setFilms] = React.useState([]);
 
-  React.useEffect(() => {
-    fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films', {
-      method: 'GET',
-      headers: {
-        'X-API-KEY': '9844d1bb-3885-4266-bc03-0281c1255cc8',
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((res) => res.json())
-      .then((obj) => setFilms(obj.items))
-      .catch((err) => console.log(err));
-  }, []);
-
+  // React.useEffect(() => {
+  //   fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films', {
+  //     method: 'GET',
+  //     headers: {
+  //       'X-API-KEY': '9844d1bb-3885-4266-bc03-0281c1255cc8',
+  //       'Content-Type': 'application/json',
+  //     },
+  //   })
+  //     .then((res) => res.json())
+  //     .then((obj) => setFilms(obj.items))
+  //     .catch((err) => console.log(err));
+  // }, []);
+  console.log(films);
   return (
     <div className={styles.movies}>
       <p className={styles.moviesFoundResult}>
-        <b>{10 ?? films.length}</b> movies found
+        <b>{films.length}</b> movies found
       </p>
       <ul className={styles.moviesList}>
-        {<p>ошибка сервера</p> ??
-          films.map(({ nameRu, year, posterUrl, kinopoiskId }) => (
-            <MoviesCard
-              key={kinopoiskId}
-              nameRu={nameRu}
-              year={year}
-              posterUrl={posterUrl}
-            />
-          ))}
+        {films.map(({ nameRu, year, posterUrl, kinopoiskId }) => (
+          <MoviesCard
+            key={kinopoiskId}
+            nameRu={nameRu}
+            year={year}
+            posterUrl={posterUrl}
+          />
+        ))}
       </ul>
     </div>
   );
