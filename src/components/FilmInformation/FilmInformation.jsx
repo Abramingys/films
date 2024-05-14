4fca71554b151c71dff3f475b53e42b6744dfddb
Vladimir Abramovich <abramingys@gmail.com>
2024-05-14 19:40:11 +0300

    component filmInformation optimisation

--- src/components/FilmInformation/FilmInformation.jsx
+++ src/components/FilmInformation/FilmInformation.jsx
@@ -1,36 +1,8 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import Loader from '../Loader/Loader';
 
 import styles from './FilmInformation.module.scss';
 
-export default function FilmInformation() {
-  const { id } = useParams();
-  const [filmDetails, setFilmDetails] = React.useState({});
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    setIsLoading(true);
-    fetch(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${id}`, {
-      method: 'GET',
-      headers: {
-        'X-API-KEY': 'cfd088f5-c917-4302-abb3-249d8da7585f',
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setFilmDetails(data);
-        setIsLoading(false);
-      });
-  }, [id]);
-
-  if (isLoading) {
-    return <Loader />;
-  }
-
+export default function FilmInformation({ filmDetails }) {
   return (
     <div className={styles.filmInformation}>
       <img
