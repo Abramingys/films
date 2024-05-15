b12ad0b5387afcd4c379add14d93b1ffd09438f7
Vladimir Abramovich <abramingys@gmail.com>
2024-05-15 19:50:51 +0300

    changed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 );
-
-{
-  /* <React.StrictMode>
-<BrowserRouter>
-  <Provider store={store}>
-    <App />
-  </Provider>
-</BrowserRouter>
-</React.StrictMode>, */
-}
