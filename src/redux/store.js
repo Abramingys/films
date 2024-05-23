384492d3fbc6819870d48524d16641943a276c5b
Vladimir Abramovich <abramingys@gmail.com>
2024-05-23 11:56:58 +0300

    added historyMiddleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './slices/authSlice';
 import favoriteReducer from './slices/favoriteSlice';
+import { historyMiddleware } from './slices/historyMiddleware';
 import historyReducer from './slices/historySlice';
 
 export const store = configureStore({
@@ -10,4 +11,6 @@ export const store = configureStore({
     favorite: favoriteReducer,
     history: historyReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(historyMiddleware),
 });
